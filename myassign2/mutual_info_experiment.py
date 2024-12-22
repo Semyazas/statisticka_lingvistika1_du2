032b7e1b553637abee8ba3116a3ebc8d8f55b8b2
Martin Kunst <martin.kunst2001@centrum.cz>
2024-12-22 16:35:39 +0100

    bugfix 1: mutual info experiment, consequent bigram -- incorrect window size and allignement

--- myassign2/mutual_info_experiment.py
+++ myassign2/mutual_info_experiment.py
@@ -14,6 +14,36 @@ def consequtive_bigrams_experiment(lines : list[str]) -> list[float]:
 
     return sorted_pmi
 
+def nonconsecutive_bigrams_experiment(lines : list[str]) -> list[float]:
+    word_counts,tuple_counts, word_triple_counts, chars, last_bigram_unigram = count_words(lines)
+    prob = Probability(word_counts,tuple_counts,word_triple_counts,chars, last_bigram_unigram)
+    prob.compute_distributions()
+
+    non_consequtive_bigrams = []
+    len_lines = len(lines) 
+
+    window_size = 3
+
+    for index,word in enumerate(lines):
+        window_left_index = 0
+        if index > window_size: 
+            window_left_index = index - window_size
+
+        if len_lines - index < window_size: 
+            window_right_index = index + window_size
+        
+        for word_prev in lines[window_left_index:index]:
+            if word_prev != word:
+                non_consequtive_bigrams.append((word_prev,word))
+
+        for word_next in lines[index+1:window_right_index+1]:
+            if word_next != word:
+                non_consequtive_bigrams.append((word,word_next))
+
+    print(non_consequtive_bigrams)
+
 if __name__ == '__main__':
     lines = read_file('input\\TEXTEN.txt')
     consequtive_bigrams_experiment(lines)
+
+    nonconsecutive_bigrams_experiment(lines)
