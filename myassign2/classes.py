2617b4e4d24acfd7dcb6327f8e06f67819b16a3f
Martin Kunst <martin.kunst2001@centrum.cz>
2024-12-30 18:31:49 +0100

    feature: implementation of merge classes 3 --NEEDS COPLETION--

--- myassign2/classes.py
+++ myassign2/classes.py
@@ -14,17 +14,42 @@ class Word_Classes_Distribution(Probability):
         self.word_to_class = {}
         self.classes_counts = {}
         self.classes_bigram_counts = {}
+        self.classes_bigram_distributions = {}
     
     def merge_classes(self) -> dict[str,int]:
         new_classes = {}
-    
+        new_word_to_class = {}
+        new_class_bigram_distribution = {}
+        new_class_bigram_counts = {}
 
-        for left_class, right_class in self.word_tuple_counts.items():
+
+        for left_class, right_class in self.classes_bigram_counts.keys():
             new_classes = self.word_to_classs.copy()
-            new_classes[tuple(left_class + right_class)] = new_classes[left_class] + new_classes[right_class] 
+            new_class = tuple(left_class + right_class)
+            new_classes[new_class] = new_classes[left_class] + new_classes[right_class] 
             new_classes.remove(left_class)
             new_classes.remove(right_class)
 
+            for history, word in self.word_tuple_counts.keys():
+                h_class = list(self.word_to_class[history])
+                w_class = list(self.word_to_class[word])
+
+                if self.word_to_class[history] == left_class:
+                    h_class = new_class
+                if self.word_to_class[word] == right_class:
+                    w_class = new_class
+
+                new_class_bigram_counts[tuple(h_class + w_class)] =
+
+                if self.word_to_class[history] == left_class and self.word_to_class[word] == right_class:
+                    new_word_to_class[word] = new_class
+                    new_word_to_class[history] = new_class
+                else:
+                    new_word_to_class[word] = self.word_to_class[history]
+                    new_word_to_class[history] = self.word_to_class[word]
+
+                
+                
 
     def greedy_A_classes(self) -> dict[str,int]:
         self.word_classes_count = self.word_counts
@@ -33,9 +58,13 @@ class Word_Classes_Distribution(Probability):
 
         return self.word_classes_count
     
-    def probability_of_bigram(self, history : str, word : str) -> float:
-        return self.classes_bigram_counts()
 
+    def probability_of_bigram(self, history : str, word : str, distr : dict, classes_counts : dict, classes_bigram_counts) -> dict:
+        w_class = self.word_to_class[word]
+        h_class = self.word_to_class[history]
+        for history, word in self.word_tuple_counts.keys():
+            distr[(h_class,w_class)] = self.word_counts(word) / classes_counts(w_class) *  classes_bigram_counts((h_class, w_class)) / classes_counts(h_class) 
+        return distr
 if __name__ == '__main__':
     lines = read_file('input_classes\\TEXTEN1.ptg')
     word_counts, word_tuple_counts, word_triple_counts, characters, last_bigram_unigram = actual_count_words(lines)
