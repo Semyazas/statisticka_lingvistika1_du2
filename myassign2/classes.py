df72df6322791bafb3dc11e72ca83878e88ea14f
Martin Kunst <martin.kunst2001@centrum.cz>
2025-02-10 15:28:11 +0100

    feature: merging 2 classes --- TODO: fix representation of classes (should be immutable)

--- myassign2/classes.py
+++ myassign2/classes.py
@@ -16,66 +16,7 @@ class Word_Classes_Distribution(Probability):
         self.classes_bigram_counts = {}
         self.classes_bigram_distributions = {}
     
-    def merge_classes(self) -> dict[str,int]:
-        new_classes = {}
-        new_word_to_class = {}
-        new_class_bigram_distribution = {}
-        new_class_bigram_counts = {}
-
-
-        for left_class, right_class in self.classes_bigram_counts.keys():
-            new_classes = self.word_to_class.copy()         
-            new_class = tuple(left_class + right_class)
-            new_classes[new_class] = new_classes[left_class] + new_classes[right_class] 
-            new_classes.remove(left_class)
-            new_classes.remove(right_class)
-
-            for history, word in self.word_tuple_counts.keys():
-                h_class = list(self.word_to_class[history])
-                w_class = list(self.word_to_class[word])
-
-                if self.word_to_class[history] == left_class:
-                    h_class = new_class
-                if self.word_to_class[word] == right_class:
-                    w_class = new_class
-
-                if tuple(h_class + w_class) not in new_class_bigram_counts:
-                    new_class_bigram_counts[tuple(h_class + w_class)] = 1 
-                else:
-                    new_class_bigram_counts[tuple(h_class + w_class)] += 1
-
-                if self.word_to_class[history] == left_class and self.word_to_class[word] == right_class:
-                    new_word_to_class[word] = new_class
-                    new_word_to_class[history] = new_class
-                else:
-                    new_word_to_class[word] = self.word_to_class[history]
-                    new_word_to_class[history] = self.word_to_class[word]
-
-            new_class_bigram_distribution = self.get_class_distribution(history,word,new_class,new_class_bigram_counts, new_class_bigram_counts)
-
-            prob = 0
-            for word in word_counts:
-                for history in word_counts:
-                    prob += self._get_probability(word, history, new_class_bigram_distribution)
-                    
-            assert prob == 1 , "Probability does not sum to 1"
-                
-    def _get_probability(self,word,history, distr) -> float:
-
-        if (history, word) in distr:
-            return distr[(history, word)]
-        return 0
-
-    def greedy_A_classes(self) -> dict[str,int]:
-        self.compute_distributions()
-
-        self.word_classes_count = self.word_counts.copy()
-        self.word_to_classes       = {key : [key] for key in self.word_counts.keys()}
-        self.classes_bigram_counts = self.word_tuple_counts.copy()
-        self.classes_bigram_distributions = self.bigram_conditional_distribution.copy()
-        self.merge_classes()
-
-        return self.word_classes_count
+   
     
     def get_class_distribution(self, history : str, word : str, distr : dict, classes_counts : dict, classes_bigram_counts) -> dict:
         w_class = self.word_to_class[word]
@@ -85,7 +26,57 @@ class Word_Classes_Distribution(Probability):
         return distr
 
 
+    def get_q_k(self,left_class, right_class):
+        pass
+
+    def merge_bigram_class_counts(self,left_class : list[str],right_class :list[str]) -> dict:
+        new_bigram_counts = self.classes_bigram_counts.copy()
+        new_class = left_class + right_class
+        new_bigram_counts[tuple(new_class,new_class)] = (
+                                                            new_bigram_counts[tuple(left_class,right_class)] + 
+                                                            new_bigram_counts[tuple(right_class,left_class)] + 
+                                                            new_bigram_counts[tuple(left_class,left_class)]  + 
+                                                            new_bigram_counts[tuple(right_class,right_class)]
+                                                        )
+        for history,word in self.classes_bigram_counts.keys():
+            if (history == left_class and word != right_class) or (history == right_class and word != left_class):
+                new_bigram_counts[tuple(new_class,word)] = new_bigram_counts[tuple(left_class,word)] + new_bigram_counts[tuple(right_class,word)]
+
+            elif  (history != left_class and word == right_class) or (history != right_class and word == left_class):
+                new_bigram_counts[tuple(history,new_class)] = new_bigram_counts[tuple(history,left_class)] + new_bigram_counts[tuple(history,right_class)]
+
+        new_bigram_counts.remove(left_class)
+        new_bigram_counts.remove(right_class)
+
+
+def assert_dicts_equal(dict1, dict2):
+    assert dict1 == dict2, f"Dictionaries are not equal! Differences: {set(dict1.items()) ^ set(dict2.items())}"
+
+def test_merge():
+    print("Testing merge")
+    wc = Word_Classes_Distribution({}, {}, {}, {})
+    
+    # Fill in the bigram counts based on the image
+    wc_classes_bigram_counts = {
+        (("c1",), ("c1",)): 10, (("c1",), ("c2",)): 2, (("c1",), ("c3",)): 0, (("c1",), ("c4",)): 1,
+        (("c2",), ("c1",)): 0,  (("c2",), ("c2",)): 0, (("c2",), ("c3",)): 5, (("c2",), ("c4",)): 2,
+        (("c3",), ("c1",)): 0,  (("c3",), ("c2",)): 2, (("c3",), ("c3",)): 0, (("c3",), ("c4",)): 3,
+        (("c4",), ("c1",)): 2,  (("c4",), ("c2",)): 3, (("c4",), ("c3",)): 0, (("c4",), ("c4",)): 0
+    }
+
+    bigram_counts = {
+        (("c1",), ("c1",)): 10, (("c1",), ("c1", "c2")): 3, (("c1",), ("c3",)): 0,
+        (("c2", "c4"), ("c1",)): 2, (("c2", "c4"), ("c2", "c4")): 5, (("c2", "c4"), ("c3",)): 5,
+        (("c3",), ("c1",)): 0, (("c3",), ("c1", "c2")): 5, (("c3",), ("c3",)): 0
+    }
+    tuple_counts = wc.merge_bigram_class_counts(["c2"],["c4"])
+    assert_dicts_equal(tuple_counts, bigram_counts)
+    print("Test successful !")
+    # Your additional test logic goes here
+
 if __name__ == '__main__':
+    test_merge()
+
     lines = read_file('input_classes\\TEXTEN1.ptg')
     word_counts, word_tuple_counts, word_triple_counts, characters, last_bigram_unigram = actual_count_words(lines[:8000])
 #    print(word_counts)
